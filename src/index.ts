4052c19a51030308b9ec04067d055248ecb557de
scherepi <scherejoaquin@gmail.com>
2025-06-05 16:14:46 -0500

    outlined command framework

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+// Main CLI logic, started by Joaquin 6/5/2025
+import { Command } from 'commander';
+const program = new Command();
+
+program
+	.name('conduit')
+	.description('Conduit - a link between worlds.')
+	.version('1.0.0');
+
+program
+	// conduit remotehost -l localPort -p remotePort -d subdomain -v verbosityLevel 
+	.argument('<remotehost>', 'the remote host to try to tunnel to')
+	.requiredOption('-l, --localPort <portNumber>', 'the local port to try to tunnel to the server', )
+	.option('-p, --remotePort <portNumber>', 'the remote port to request from the server (optional)')
+	.option('-d, --subdomain <subdomain>', 'the subdomain to request from the server. -p and -d are mutually exclusive.')
+	.option('-v, --verbosity <level>', 'set verbosity level', '3')
+
+// conduit server <bindAddress> -t tunnelAddress -m minimumPort -M maximumPort 
+program.command('server')
+	.description('Base functionality.')
+	.argument('<bindAddress>', 'the address to bind the server to', '0.0.0.0')
+	.option('-t, --tunnelBind', 'the address to bind tunnels to.', '0.0.0.0')
+	.option('-m, --minPort', 'the minimum port of the port range on which you want to allow incoming conections', '1024')
+	.option('-M, --maxPort', 'the maximum port of the port range on which you want to allow incoming connections', '65535')
+
+
+program.parse();
